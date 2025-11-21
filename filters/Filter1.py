79b91ff7826ea5e7f2e0b9ba518be57c5f612a9b
AdamDavis08 <hristijangajdov@gmail.com>
2025-11-21 22:33:54 +0100

    Update values till today from last date

--- filters/Filter1.py
+++ filters/Filter1.py
@@ -1,13 +1,30 @@
 from coinpaprika import client as Coinpaprika
 from filters.Filter import *
+
+from db_controller.db import Database
+
 import pandas as pd
 
+def get_symbols_from_coinpaprika():
+    client = Coinpaprika.Client()
+    df = pd.DataFrame(client.coins())
+
+    df = df[df["is_active"] == True] #only active coinst
+    df = df.drop_duplicates(subset="symbol") #drop duplicate symbols
+
+    return df[['symbol']].head(1100)
+
+def get_symbols_from_db(db):
+    rows = db.fetchall('SELECT DISTINCT symbol FROM market_data;')
+    df = pd.DataFrame(rows)
+    return df
+
 class Top1000CoinsFilter(Filter):
+    def __init__(self):
+        self.db = Database()
+
     def process(self, data):
-        client = Coinpaprika.Client()
-        df = pd.DataFrame(client.coins())
-
-        df = df[df["is_active"] == True] #only active coinst
-        df = df.drop_duplicates(subset="symbol") #drop duplicate symbols
-
-        return df.head(1000)
+        if self.db.is_empty():
+            return get_symbols_from_coinpaprika()
+        else:
+            return get_symbols_from_db(self.db)
