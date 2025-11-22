28c420975203560f718a50a35d5870c0e299c91e
Kiril Veljanoski <kiril.angeli@gmail.com>
2025-11-22 13:11:14 +0100

    coingecko fetch

--- filters/Filter1.py
+++ filters/Filter1.py
@@ -4,19 +4,20 @@ from filters.Filter import *
 import pandas as pd
 import requests
 import re
+from bs4 import BeautifulSoup
 
 
 class Top1000CoinsFilter(Filter):
     # Gets extra coins just to have spare after filtering we can remove the spare if coins > 1000
     URLs = [
-        "https://query1.finance.yahoo.com/v1/finance/screener/predefined/saved?count=250&formatted=true&scrIds=ALL_CRYPTOCURRENCIES_US&sortField=intradaymarketcap&sortType=desc&start=0&useRecordsResponse=true&fields=ticker%2ClogoUrl%2Csymbol%2ClongName%2Csparkline%2CshortName%2CregularMarketPrice%2CregularMarketChange%2CregularMarketChangePercent%2CmarketCap%2CregularMarketVolume%2Cvolume24Hr%2CvolumeAllCurrencies%2CcirculatingSupply%2CfiftyTwoWeekChangePercent%2CfiftyTwoWeekRange&lang=en-US&region=US",
-        "https://query1.finance.yahoo.com/v1/finance/screener/predefined/saved?count=250&formatted=true&scrIds=ALL_CRYPTOCURRENCIES_US&sortField=intradaymarketcap&sortType=desc&start=250&useRecordsResponse=true&fields=ticker%2ClogoUrl%2Csymbol%2ClongName%2Csparkline%2CshortName%2CregularMarketPrice%2CregularMarketChange%2CregularMarketChangePercent%2CmarketCap%2CregularMarketVolume%2Cvolume24Hr%2CvolumeAllCurrencies%2CcirculatingSupply%2CfiftyTwoWeekChangePercent%2CfiftyTwoWeekRange&lang=en-US&region=US",
-        "https://query1.finance.yahoo.com/v1/finance/screener/predefined/saved?count=250&formatted=true&scrIds=ALL_CRYPTOCURRENCIES_US&sortField=intradaymarketcap&sortType=desc&start=500&useRecordsResponse=true&fields=ticker%2ClogoUrl%2Csymbol%2ClongName%2Csparkline%2CshortName%2CregularMarketPrice%2CregularMarketChange%2CregularMarketChangePercent%2CmarketCap%2CregularMarketVolume%2Cvolume24Hr%2CvolumeAllCurrencies%2CcirculatingSupply%2CfiftyTwoWeekChangePercent%2CfiftyTwoWeekRange&lang=en-US&region=US",
-        "https://query1.finance.yahoo.com/v1/finance/screener/predefined/saved?count=250&formatted=true&scrIds=ALL_CRYPTOCURRENCIES_US&sortField=intradaymarketcap&sortType=desc&start=750&useRecordsResponse=true&fields=ticker%2ClogoUrl%2Csymbol%2ClongName%2Csparkline%2CshortName%2CregularMarketPrice%2CregularMarketChange%2CregularMarketChangePercent%2CmarketCap%2CregularMarketVolume%2Cvolume24Hr%2CvolumeAllCurrencies%2CcirculatingSupply%2CfiftyTwoWeekChangePercent%2CfiftyTwoWeekRange&lang=en-US&region=US",
-        "https://query1.finance.yahoo.com/v1/finance/screener/predefined/saved?count=250&formatted=true&scrIds=ALL_CRYPTOCURRENCIES_US&sortField=intradaymarketcap&sortType=desc&start=1000&useRecordsResponse=true&fields=ticker%2ClogoUrl%2Csymbol%2ClongName%2Csparkline%2CshortName%2CregularMarketPrice%2CregularMarketChange%2CregularMarketChangePercent%2CmarketCap%2CregularMarketVolume%2Cvolume24Hr%2CvolumeAllCurrencies%2CcirculatingSupply%2CfiftyTwoWeekChangePercent%2CfiftyTwoWeekRange&lang=en-US&region=US",
-        "https://query1.finance.yahoo.com/v1/finance/screener/predefined/saved?count=250&formatted=true&scrIds=ALL_CRYPTOCURRENCIES_US&sortField=intradaymarketcap&sortType=desc&start=1250&useRecordsResponse=true&fields=ticker%2ClogoUrl%2Csymbol%2ClongName%2Csparkline%2CshortName%2CregularMarketPrice%2CregularMarketChange%2CregularMarketChangePercent%2CmarketCap%2CregularMarketVolume%2Cvolume24Hr%2CvolumeAllCurrencies%2CcirculatingSupply%2CfiftyTwoWeekChangePercent%2CfiftyTwoWeekRange&lang=en-US&region=US",
+        "https://www.coingecko.com/?items=300",
+        "https://www.coingecko.com/?page=2&items=300",
+        "https://www.coingecko.com/?page=3&items=300",
+        "https://www.coingecko.com/?page=4&items=300",
+        "https://www.coingecko.com/?page=5&items=300",
+        # "https://www.coingecko.com/?page=5&items=200",
+        # "https://www.coingecko.com/?page=6&items=200",
     ]
-
 
     @staticmethod
     def fetch_url(url):
@@ -27,9 +28,16 @@ class Top1000CoinsFilter(Filter):
                               "Chrome/120.0.0.0 Safari/537.36"
             }
             resp = requests.get(url, headers=HEADERS, timeout=10)
-            resp.raise_for_status()
-            data = resp.json()
-            return data["finance"]["result"][0]["records"]
+            bs = BeautifulSoup(resp.text, "html.parser")
+            data = []
+            rows = bs.select("table.gecko-homepage-coin-table > tbody > tr")
+            for row in rows:
+                cells = row.select("td")
+                index = int(cells[1].text)
+                symbol = cells[2].select_one("a > div > div > div").text.strip()
+                vol = float(cells[9].select_one("span").text[1:].replace(",", ""))
+                data.append({"index": index, "symbol": symbol, "volume": vol})
+            return data
         except Exception as e:
             print(f"[ERROR] Failed to fetch {url}: {e}")
             return []
@@ -49,7 +57,7 @@ class Top1000CoinsFilter(Filter):
     def filter_liquid_coins(self, coins, min_liquidity=1_000_000):
         filtered = []
         for c in coins:
-            volume = c.get("volume24Hr", {}).get("raw", 0)
+            volume = c.get("volume", 0)
             if volume >= min_liquidity:
                 filtered.append(c)
         return filtered
@@ -59,15 +67,17 @@ class Top1000CoinsFilter(Filter):
         # df1 = pd.DataFrame(coins1)
         # df1.drop(columns=["rank", "is_new", "is_active", "type"], inplace=True)
         coins = self.get_top_coins()
-        print(len(coins))
         liquid_coins = self.filter_liquid_coins(coins)
+        print(len(liquid_coins))
         df = pd.DataFrame(liquid_coins)
-        df = df[["ticker"]]
+        # df = df[["ticker"]]
 
         # df = df[df["is_active"] == True]  # only active coins - ova nz kako da go proverime so yfinance
-        df = df.drop_duplicates(subset="ticker")  # drop duplicate symbols
+        df = df.drop_duplicates(subset="symbol")  # drop duplicate symbols
         df = df.head(1000)
-        df["symbol"] = df["ticker"].apply(lambda text: re.split("-", text)[0])
+        print(df.info())
+        # df["symbol"] = df["ticker"].apply(lambda text: re.split("-", text)[0])
         # df_intersection = df[df["symbol"].isin(df1["symbol"])]
+        # df.to_csv("./test.csv")
         # print(df.info())
         return df
