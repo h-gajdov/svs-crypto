67ddda92779efa61b873542a62d31cd74abfc340
AdamDavis08 <hristijangajdov@gmail.com>
2025-12-01 00:28:55 +0100

    Initial front-end for dashboard

---
+++ Домашна 2/tech prototype/src/main/java/com/svsbrains/svscrypto/web/controler/DashboardController.java
@@ -0,0 +1,16 @@
+package com.svsbrains.svscrypto.web.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+    @GetMapping
+    public String getDashboard(Model model) {
+        model.addAttribute("bodyContent", "dashboard");
+        return "master-template";
+    }
+}
