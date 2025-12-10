ca74b71c657eeb05e7856391650fc16d3fabe8ac
hristijan.gajdov <hristijan.gajdov@students.finki.ukim.mk>
2025-12-10 01:35:16 +0100

    Get transactions count in on-chain metrics

--- Домашна 3/analysis-service/onchain/onchain_metrics.py
+++ Домашна 3/analysis-service/onchain/onchain_metrics.py
@@ -1,21 +1,32 @@
 import requests
 from datetime import datetime
 
-def get_address_count(symbol, daysBefore=1):
-    url = f'https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets={symbol.lower()}&metrics=AdrActCnt'
+def get_coinmetrics_data(symbol, daysBefore, metrics):
+    url = f'https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets={symbol.lower()}&metrics={metrics}'
     response = requests.get(url)
     data = response.json()
     if "error" in data:
         raise RuntimeError(f"API returned an error: {data['error']}")
-    
-    result = data["data"][-daysBefore:]
-    for entry in result:
+
+    return data['data'][-daysBefore:]
+
+def parse_coinmetrics_data(data):
+    for entry in data:
         entry['asset'] = entry['asset'].upper()
         time_str = entry['time'] 
         time_str = time_str.split('.')[0] + "Z"
         dt = datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%SZ")
         entry['timestamp'] = dt.timestamp()
-    return result
+    return data
+
+def get_address_count(symbol, daysBefore=1):   
+    result = get_coinmetrics_data(symbol, daysBefore, 'AdrActCnt')
+    return parse_coinmetrics_data(result)
+
+def get_transactions_count(symbol, daysBefore=1):
+    result = get_coinmetrics_data(symbol, daysBefore, 'TxCnt')
+    return parse_coinmetrics_data(result)
 
 if __name__ == '__main__':
-    print(get_address_count('BTC'))
+    print(get_address_count('USDC'))
+    print(get_transactions_count('USDC'))
