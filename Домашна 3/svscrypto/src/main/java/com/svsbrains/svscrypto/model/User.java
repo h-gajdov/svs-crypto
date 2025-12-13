1302755759ca0c9e0d105a7d255f94e085524f9b
Kiril Veljanoski <kveljanoski18@gmail.com>
2025-12-13 18:32:15 +0100

    fixed dark mode

--- Домашна 3/svscrypto/src/main/java/com/svsbrains/svscrypto/model/User.java
+++ Домашна 3/svscrypto/src/main/java/com/svsbrains/svscrypto/model/User.java
@@ -14,7 +14,6 @@ import java.util.Set;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "app_user")
 public class User {
@@ -68,29 +67,30 @@ public class User {
 
     @ElementCollection(fetch = FetchType.EAGER)
     private List<String> coins;
-    public User(){
-        this.username="";
-        this.first_name="";
-        this.last_name="";
-        this.email="";
-        this.password="";
-        this.coins=new LinkedList<>();
+
+    public User() {
+        this.username = "";
+        this.first_name = "";
+        this.last_name = "";
+        this.email = "";
+        this.password = "";
+        this.coins = new LinkedList<>();
     }
 
     public User(String username, String firstName, String lastName, String email, String password) {
-        this.username=username;
-        this.first_name=firstName;
-        this.last_name=lastName;
-        this.email=email;
-        this.password=password;
-        this.coins=new LinkedList<>();
+        this.username = username;
+        this.first_name = firstName;
+        this.last_name = lastName;
+        this.email = email;
+        this.password = password;
+        this.coins = new LinkedList<>();
     }
 
-    public void removeCoin(String c){
+    public void removeCoin(String c) {
         this.coins.remove(c);
     }
 
-    public void addCoin(String c){
+    public void addCoin(String c) {
         this.coins.add(c);
     }
 
